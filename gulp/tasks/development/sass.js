174c98aed19d1fee1fa1d9a285dc4fdc613f0472
Blake <blakersim@gmail.com>
2014-10-22 17:11:47 +0200

    Refactor sass.js task for improved maintainability

--- gulp/tasks/development/sass.js
+++ gulp/tasks/development/sass.js
@@ -12,13 +12,8 @@ var config       = require('../../config');
  * Build sourcemaps
  */
 gulp.task('sass', function() {
-  var sassConfig = {
-    noCache: config.sass.options.noCache,
-    compass: config.sass.options.compass,
-    bundleExec: config.sass.options.bundleExec,
-    sourcemapPath: config.sass.options.sourcemapPath,
-    onError: browsersync.notify
-  };
+  var sassConfig     = config.sass.options;
+  sassConfig.onError = browsersync.notify;
 
   // Don’t write sourcemaps of sourcemaps
   var filter = gulpFilter(['*.css', '!*.map']);
@@ -29,10 +24,7 @@ gulp.task('sass', function() {
     .pipe(plumber())
     .pipe(sass(sassConfig))
     .pipe(sourcemaps.init())
-    .pipe(autoprefixer({
-      browsers: config.autoprefixer.browsers,
-      cascade: config.autoprefixer.cascade
-    }))
+    .pipe(autoprefixer(config.autoprefixer))
     .pipe(filter) // Don’t write sourcemaps of sourcemaps
     .pipe(sourcemaps.write('.', { includeContent: false }))
     .pipe(filter.restore()) // Restore original files
