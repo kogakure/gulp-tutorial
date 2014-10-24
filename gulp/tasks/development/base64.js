fab25b376e8108f0641754bc70ba23995300c7c5
Stefan Imhoff <kogakure@gmail.com>
2014-10-24 08:05:37 +0200

    Refactor base64.js task for improved maintainability

--- gulp/tasks/development/base64.js
+++ gulp/tasks/development/base64.js
@@ -1,17 +1,14 @@
 var gulp   = require('gulp');
 var base64 = require('gulp-base64');
-var config = require('../../config').base64;
+var config = require('../../config');
 
 /**
- * Replace URLs in CSS fies with base64 encoded data
+ * Replace urls in CSS fies with base64 encoded data
  */
 gulp.task('base64', ['sass'], function() {
-  return gulp.src(config.src)
-    .pipe(base64({
-      baseDir: config.options.baseDir,
-      extensions: config.options.extensions,
-      debug: config.options.debug,
-      maxImageSize: config.options.maxImageSize // bytes
-    }))
-    .pipe(gulp.dest(config.dest));
+  var base64Config = config.base64.options;
+
+  return gulp.src(config.base64.src)
+    .pipe(base64(base64Config))
+    .pipe(gulp.dest(config.base64.dest));
 });
